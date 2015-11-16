33d13f752a151092e4caba943f1d64c0143ae0b3
Kghbln <karsten@hoffmeyer.info>
2015-11-16 19:22:10 +0100

    Improved file documenation as well as license added
    * Improved file documentation
    * Added COPYING
    * Added license information
    * Swichted to semver
    
    Change-Id: I9a41599ce6db78224a4e9c923b4012a2efe93933

--- NoBogusUserpages.class.php
+++ NoBogusUserpages.class.php
@@ -4,22 +4,11 @@
  *
  * @file
  * @ingroup Extensions
- * @author Daniel Friesen (https://www.mediawiki.org/wiki/User:Dantman) <mediawiki@danielfriesen.name>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * @author Daniel Friesen (Dantman) <mediawiki@danielfriesen.name>
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  *
- * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
 class NoBogusUserpages {
--- NoBogusUserpages.php
+++ NoBogusUserpages.php
@@ -1,46 +1,49 @@
 <?php
 /**
- * NoBogusUserpages
- * @package NoBogusUserpages
- * @author Daniel Friesen (https://www.mediawiki.org/wiki/User:Dantman) <mediawiki@danielfriesen.name>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * The NoBogusUserpages exension to MediaWiki allows only privileged users to create userpages
+ * for non-existent users
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * @link https://www.mediawiki.org/wiki/Extension:NoBogusUserpages Documentation
+ * @link https://www.mediawiki.org/wiki/Extension_talk:NoBogusUserpages Support
+ * @link https://git.wikimedia.org/summary/mediawiki%2Fextensions%2FNoBogusUserpages.git Source Code
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * @file
+ * @ingroup Extensions
+ * @package MediaWiki
  *
- * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * @author Daniel Friesen (Dantman) <mediawiki@danielfriesen.name>
+ *
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
+// Ensure that the script cannot be executed outside of MediaWiki
 if ( !defined( 'MEDIAWIKI' ) ) {
-	die( "This is an extension to the MediaWiki package and cannot be run standalone." );
+	die( "This is an extension to MediaWiki and cannot be run standalone." );
 }
 
+// Display extension's information on "Special:Version"
 $wgExtensionCredits['antispam'][] = array(
 	'path' => __FILE__,
 	'name' => 'NoBogusUserpages',
-	'version' => '1.1',
+	'version' => '1.1.0',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:NoBogusUserpages',
-	'author' => '[https://www.mediawiki.org/wiki/User:Dantman Daniel Friesen] [mailto:Daniel%20Friesen%20%3Cmediawiki@danielfriesen.name%3E <mediawiki@danielfriesen.name>]',
+	'author' => array(
+		'[https://www.mediawiki.org/wiki/User:Dantman Daniel Friesen]',
+		'...'
+	),
 	'descriptionmsg' => 'nobogususerpages-desc',
+	'license-name' => 'GPL-2.0+'
 );
 
+// Load extension's class
 $wgAutoloadClasses['NoBogusUserpages'] = __DIR__ . '/NoBogusUserpages.class.php';
 
-// Internationalization files
+// Load internationalization files
 $wgMessagesDirs['NoBogusUserpages'] = __DIR__ . '/i18n';
 
-// Add missing permission
+// Add user permission
 $wgAvailableRights[] = 'createbogususerpage';
 $wgGroupPermissions['sysop']['createbogususerpage'] = true;
 
-// Hook
+// Register hook
 $wgHooks['getUserPermissionsErrors'][] = 'NoBogusUserpages::onGetUserPermissionsErrors';
